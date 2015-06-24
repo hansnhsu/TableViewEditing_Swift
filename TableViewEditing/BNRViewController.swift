ca93735531d03abb992a53422ca14e702cdcb0b9
Hansen Hsu <hansnhsu@gmail.com>
2015-06-24 19:05:30 -0400

    Add Swift file with empty extension for now, Bridging Header

---
+++ TableViewEditing/BNRViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BNRViewController.swift
+//  TableViewEditing
+//
+//  Created by Hansen Hsu on 6/24/15.
+//  Copyright (c) 2015 Hansen Hsu. All rights reserved.
+//
+
+import UIKit
+
+extension BNRViewController {
+    
+}
